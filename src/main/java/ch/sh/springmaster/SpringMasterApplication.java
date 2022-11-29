99c72895db8d232f7737d1864178c5e89973dd65
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-11-29 17:09:28 +0600

    Initial commit

---
+++ src/main/java/ch/sh/springmaster/SpringMasterApplication.java
@@ -0,0 +1,13 @@
+package ch.sh.springmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/sh/springmaster/SpringMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sh.springmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
