41952a38c5abce6d80342c02ef7762404d28297e
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-11-30 17:35:54 +0600

    add @Bean annotation

--- src/main/java/ch/sh/springmaster/customer/CustomerConfiguration.java
+++ src/main/java/ch/sh/springmaster/customer/CustomerConfiguration.java
@@ -1,5 +1,6 @@
 package ch.sh.springmaster.customer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CustomerConfiguration {
 
+    @Value("${app.useFakeCustomerRepo:false}")
+    private Boolean userFakeCustomerRepo;
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
             System.out.println("Command line runner hooray");
         };
     }
+
+    @Bean
+    CustomerRepo customerRepo(){
+        System.out.println("useFakeCustomerRepo = " + userFakeCustomerRepo);
+        return userFakeCustomerRepo ?
+                new CustomerFakeRepository() :
+                new CustomerRepository();
+    }
+
 }
--- src/main/java/ch/sh/springmaster/customer/CustomerRepository.java
+++ src/main/java/ch/sh/springmaster/customer/CustomerRepository.java
@@ -1,13 +1,8 @@
 package ch.sh.springmaster.customer;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Repository;
-
 import java.util.Collections;
 import java.util.List;
 
-@Repository
-@Primary
 public class CustomerRepository implements CustomerRepo {
 
     @Override
@@ -17,4 +12,5 @@ public class CustomerRepository implements CustomerRepo {
                 new Customer(1L, "TODO. Implement real DB")
         );
     }
+
 }
