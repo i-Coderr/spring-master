f00ad6c1ab8e36b160dceaf834e161bb9b2b6bee
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-11-30 15:26:49 +0600

    add Dependency injection

--- src/main/java/ch/sh/springmaster/customer/CustomerController.java
+++ src/main/java/ch/sh/springmaster/customer/CustomerController.java
@@ -1,13 +1,22 @@
 package ch.sh.springmaster.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+    private final CustomerService service;
+
+    @Autowired
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
+
+
     @GetMapping
     Customer getCustomer(){
-        return new Customer(1L, "Chyngyz Sharshekeev");
+        return service.getCustomer();
     }
 }
--- src/main/java/ch/sh/springmaster/customer/CustomerService.java
+++ src/main/java/ch/sh/springmaster/customer/CustomerService.java
@@ -1,4 +1,12 @@
 package ch.sh.springmaster.customer;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    Customer getCustomer(){
+        return new Customer(1L, "Chyngyz Sharshekeev");
+    }
+
 }
