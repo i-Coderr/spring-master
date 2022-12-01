6ee4ca3245c373a6b87a24812b254bdb3165c8e9
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-12-01 15:20:05 +0600

    add password to CustomerRepository

--- src/main/java/ch/sh/springmaster/customer/CustomerRepository.java
+++ src/main/java/ch/sh/springmaster/customer/CustomerRepository.java
@@ -9,7 +9,7 @@ public class CustomerRepository implements CustomerRepo {
     public List<Customer> getCustomers() {
         //TODO connect to real DB
         return Collections.singletonList(
-                new Customer(1L, "TODO. Implement real DB", password)
+                new Customer(1L, "TODO. Implement real DB", "todo")
         );
     }
 
