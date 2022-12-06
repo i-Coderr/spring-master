0b42327ba56fc06a5595933bd6cfa80be0a35f55
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-12-06 12:42:47 +0600

    add @ControllerAdvice and @ExceptionHandler

--- src/main/java/ch/sh/springmaster/exception/ApiExceptionHandler.java
+++ src/main/java/ch/sh/springmaster/exception/ApiExceptionHandler.java
@@ -2,11 +2,15 @@ package ch.sh.springmaster.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZonedDateTime;
 
-public class ExceptionHandler {
+@ControllerAdvice
+public class ApiExceptionHandler {
 
+    @ExceptionHandler(value = ApiRequestException.class)
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
         ApiException apiException = new ApiException(e.getMessage(), e, HttpStatus.BAD_REQUEST, ZonedDateTime.now());
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
