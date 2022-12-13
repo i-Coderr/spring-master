9b52c6007c26eb6f2289b1553fac13e4bedef11a
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2022-12-13 15:48:45 +0600

    add @EnableFeignClients annotation

--- src/main/java/ch/sh/springmaster/SpringMasterApplication.java
+++ src/main/java/ch/sh/springmaster/SpringMasterApplication.java
@@ -2,8 +2,10 @@ package ch.sh.springmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringMasterApplication {
 
 	public static void main(String[] args) {
